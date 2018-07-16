0d54e8219c148302ac379c972a590f847162963d
John Kirkwood <johnkirkwood@Johns-iMac.local>
2018-07-15 23:28:32 -0400

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,36 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-	entry: './src/app.js',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [{
-			loader: 'babel-loader',
-			test: /\.js$/,
-			exclude: /node_modules/
-		}, {
-			test: /\.s?css$/,
-			use: [
-				'style-loader',
-				'css-loader',
-				'sass-loader'
-			]
-		}]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true //return root for all 404 routes
-	}
-};
+module.exports = (env, argv) => {
+	const isProd = env === 'production';
+
+	return {
+		entry: './src/app.js',
+		output: {
+			path: path.join(__dirname, 'public'),
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			}, {
+				test: /\.s?css$/,
+				use: [
+					isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			}]
+		},
+		plugins: [
+			new MiniCssExtractPlugin({filename:'styles.css'})
+		],
+		devtool: isProd ? 'source-map' : 'cheap-module-eval-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true //return root for all 404 routes
+		}
+	};
+}
