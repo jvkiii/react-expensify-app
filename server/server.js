abb75d6fdf5a62c7cd231d49d00a73a9dbee1264
John Kirkwood <johnkirkwood@Johns-iMac.local>
2018-08-19 22:33:02 -0400

    Setup production build and server for heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000; //heroku provides random port
 
 app.use(express.static(publicPath));
 
@@ -9,6 +10,6 @@ app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html')); //ridirect all routes back to index.html
 });
 
-app.listen(3000, () => {
-   console.log('Server running: http://localhost:3000');
+app.listen(port, () => {
+   console.log(`Server running: http://localhost:${port}`);
 });
